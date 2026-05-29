673d42955b9642aef405378a1c34f2fe31766f72
Abhishek <abhi@dev.com>
2026-05-29 11:49:23 +0530

    GH-7 : Implemented global exception handler for validation failed cases along with integration test

---
+++ src/main/java/com/amalvadkar/ums/common/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.amalvadkar.ums.common.exceptions;
+
+import com.amalvadkar.ums.common.model.response.CustomResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest()
+                .body(CustomResponse.badRequest(extractValidationErrorMessages(ex)));
+    }
+
+    private static List<String> extractValidationErrorMessages(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+    }
+
+}
--- src/main/java/com/amalvadkar/ums/common/model/response/CustomResponse.java
+++ src/main/java/com/amalvadkar/ums/common/model/response/CustomResponse.java
@@ -1,25 +1,32 @@
 package com.amalvadkar.ums.common.model.response;
 
 import com.amalvadkar.ums.common.enums.UmsResponseMessageEnum;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
-import static com.amalvadkar.ums.common.enums.UmsResponseStatusEnum.CREATED;
-import static com.amalvadkar.ums.common.enums.UmsResponseStatusEnum.SUCCESS;
+import java.util.List;
+
+import static com.amalvadkar.ums.common.enums.UmsResponseStatusEnum.*;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @Data
 @Builder
+@JsonInclude(NON_NULL)
 public class CustomResponse {
     private Object data;
     private String message;
     private int code;
     private String status;
+    private boolean success;
+    private List<String> errors;
 
     public static CustomResponse success(Object data, UmsResponseMessageEnum message){
         return CustomResponse.builder()
                 .data(data)
                 .message(message.value())
                 .code(SUCCESS.code())
+                .success(true)
                 .status(SUCCESS.name())
                 .build();
     }
@@ -29,7 +36,17 @@ public class CustomResponse {
                 .data(data)
                 .message(message.value())
                 .code(CREATED.code())
+                .success(true)
                 .status(CREATED.name())
+                .build();
+    }
+
+    public static CustomResponse badRequest(List<String> errorMessages) {
+        return CustomResponse.builder()
+                .code(BAD_REQUEST.code())
+                .success(false)
+                .status(BAD_REQUEST.name())
+                .errors(errorMessages)
                 .build();
     }
 }
