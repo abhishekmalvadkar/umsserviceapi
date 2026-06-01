dc5adc761d674be64df9672f762fd74dc2046dd1
Abhishek <abhi@dev.com>
2026-06-01 11:44:51 +0530

    GH-9 : Continuous logging

--- src/main/java/com/amalvadkar/ums/url/services/CheckSlugService.java
+++ src/main/java/com/amalvadkar/ums/url/services/CheckSlugService.java
@@ -4,6 +4,7 @@ package com.amalvadkar.ums.url.services;
 import com.amalvadkar.ums.common.model.response.CustomResponse;
 import com.amalvadkar.ums.url.repositories.UrlRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +12,7 @@ import java.util.Map;
 
 import static com.amalvadkar.ums.common.enums.UmsResponseMessageEnum.CHECKED_SUCCESSFULLY;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -19,8 +21,10 @@ public class CheckSlugService {
     private final UrlRepo urlRepo;
 
     public CustomResponse check(String slug) {
+        boolean slugAlreadyExists = urlRepo.existsBySlug(slug);
+        log.debug("Slug already exists :: {}", slugAlreadyExists);
         return CustomResponse.success(
-                Map.of("slugAlreadyExists", urlRepo.existsBySlug(slug)),
+                Map.of("slugAlreadyExists", slugAlreadyExists),
                 CHECKED_SUCCESSFULLY
         );
     }
