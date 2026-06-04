1a458af776437ed471195622293548afd2c0309c
Abhishek <abhi@dev.com>
2026-06-04 12:07:46 +0530

    GH-13 : Added some logs for me

--- src/main/java/com/amalvadkar/ums/url/controllers/VisitUrlController.java
+++ src/main/java/com/amalvadkar/ums/url/controllers/VisitUrlController.java
@@ -3,12 +3,14 @@ package com.amalvadkar.ums.url.controllers;
 import com.amalvadkar.ums.url.models.response.VisitUrlResponse;
 import com.amalvadkar.ums.url.services.VisitUrlService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/api/ums/urls")
 @RequiredArgsConstructor
@@ -21,17 +23,19 @@ public class VisitUrlController {
     public String visitUrl(@PathVariable String slug, Model model) {
         VisitUrlResponse visitUrlResponse = visitUrlService.visitUrl(slug);
         return switch (visitUrlResponse.urlStatusEnum()) {
-            case VALID_SLUG -> forValidSlug(visitUrlResponse);
-            case UNKNOWN_SLUG, INACTIVE_SLUG -> forNotValidSlug(visitUrlResponse, model);
+            case VALID_SLUG -> forValidSlug(visitUrlResponse, slug);
+            case UNKNOWN_SLUG, INACTIVE_SLUG -> forNotValidSlug(visitUrlResponse, model, slug);
         };
     }
 
-    private static String forNotValidSlug(VisitUrlResponse visitUrlResponse, Model model) {
+    private static String forNotValidSlug(VisitUrlResponse visitUrlResponse, Model model, String slug) {
+        log.debug("Slug {} is not valid, reason :: {}", slug, visitUrlResponse.urlStatusEnum().message());
         model.addAttribute("message", visitUrlResponse.urlStatusEnum().message());
         return "visit-url-error";
     }
 
-    private static String forValidSlug(VisitUrlResponse visitUrlResponse) {
+    private static String forValidSlug(VisitUrlResponse visitUrlResponse, String slug) {
+        log.debug("Slug {} is valid, sending original url", slug);
         return "redirect:" + visitUrlResponse.originalUrl();
     }
 
